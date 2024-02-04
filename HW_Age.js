dd561b07f371ac167da5efac804c2d957318f04a
Maria <marianeuyazvimaa@gmail.com>
2024-02-04 20:19:48 +0200

    04.02.2024

--- HW_Age.js
+++ HW_Age.js
@@ -1,8 +1,24 @@
-let num = promt ('Хєй, друже! Скільки тобі років?');
-if (num == 1) 
+let num = prompt ('Хєй, друже! Скільки тобі років?');
+let realnum = Number(num);
+let remainder_division_10 = realnum % 10;
+/**для наших довгожителів **/
+let remainder_division_100 = realnum % 100; 
+
+if (typeof(realnum) == 'number' && realnum > 0)
 {
-    console.log('Ого, аж цілий рік!')
-}
-    else{
-        console.log('А давай ти спробуєш ввести ще раз і в цей раз це буде ціле позитивне число, як і ми з тобоюю :3');
+    if (remainder_division_10 === 1 && remainder_division_100 !== 11 )
+    {
+        console.log(`${realnum} рік`)
+    } else if ((remainder_division_10 >= 2 && remainder_division_10 <= 4) && !(remainder_division_100 >= 2 && remainder_division_100 <= 4))
+    {
+        console.log(`${realnum} роки`)
+    } else {
+        console.log(`${realnum} років`)
     }
+}
+ 
+else 
+{
+    console.log('А давай ти спробуєш ввести ще раз і в цей раз це буде ціле позитивне число, як і ми з тобоюю :3');
+}
+
